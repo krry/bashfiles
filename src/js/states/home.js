4fb606a0692c4ce5b91d2a643f8a64d6c2787356
Jesse LaRue <lazaruslarue@gmail.com>
2014-10-23 18:10:27 -0700

    refactors

--- src/js/states/home.js
+++
@@ -1,7 +0,0 @@
-angular.module('states.home',[]).config( function ($stateProvider) {
-  $stateProvider.state("home", {
-    url: "/",
-    controller: "HomeCtrl",
-    templateUrl: "templates/states/home/home.html",
-  });
-});
---
+++ src/js/steps/home.js
@@ -0,0 +1,21 @@
+angular.module('steps.home',[]).config( function ($stateProvider) {
+  $stateProvider.state("home", {
+    url: "/",
+    controller: "HomeCtrl",
+    templateUrl: "templates/steps/home/home.html",
+    // views: {
+    // 	'header': {
+    // 		templateUrl: 'templates/steps/home/header.html',
+    //     controller:  "",
+    // 	},
+    // 	'main': {
+    // 		templateUrl: 'templates/steps/plan/planContent.html',
+    //     controller:  "",
+    // 	},
+    // 	'footer': {
+    // 		templateUrl: 'templates/steps/plan/planContent.html',
+    //     controller:  "",
+    // 	},
+    },
+  });
+});
