8ea1a35d5b07c7bc2ded9c0ccddcb462ab6448de
krry <krry@me.com>
2014-10-24 13:09:09 -0700

    keeping concatted all.css link tag in index.html

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,18 +1,34 @@
-var gulp = require('gulp')
-var changed = require('gulp-changed')
+var gulp    = require('gulp')
+var concat  = require('gulp-concat')
+var rename  = require('gulp-rename')
+var prefix  = require('gulp-autoprefixer')
+var plumber = require('gulp-plumber')
+
+var sass    = require('gulp-sass')
+var neat    = require('node-neat').includePaths
+var minCSS  = require('gulp-minify-css')
 
 var handleErrors = require('../util/handleErrors')
 
-var cssSrc = [
-  './src/css/*'
+var styleSrc  = [
+  './src/css/**/*.scss',
+  './lib/normalize.css'
 ]
 
-var publicCss = './public/css/'
+var stylePub = './public/css/'
 
 gulp.task('styles', function(){
-  return gulp.src(cssSrc)
-        .pipe(changed(publicCss))
-        .on('error', handleErrors)
+  return gulp.src(styleSrc)
+        .pipe(plumber({
+            errorHandler: handleErrors
+        }))
+        .pipe(sass({
+            includePaths: ['styleSrc'].concat(neat)
+        }))
+        .pipe(prefix(['ie 9','last 2 versions', '> 5%'], { cascade: true }))
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest(publicCss))
+        .pipe(minCSS({keepBreaks: false}))
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(publicCss))
 })
-
