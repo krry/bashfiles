f8ffb679f4e8f5e24ee0ba7a19d6e06d7cb0a453
Jesse LaRue <lazaruslarue@gmail.com>
2014-10-28 20:06:05 -0700

    so much commiting!"

--- src/js/steps/configure.js
+++ src/js/steps/configure.js
@@ -3,50 +3,23 @@ angular.module('steps.configure',[]).config( function ($stateProvider) {
   // specifics for for this state
   var stateName = 'configure';
   var templateUrl = 'templates/';
-  var stepsUrl = 'steps/';
+  var stepsUrl = 'stages/';
   var baseUrl = templateUrl + stepsUrl + stateName + '/';
 
-  var steps = [ 
-    { step: 'zoom-lock-roof', url: baseUrl + 'zoom.html'   },
-    { step: 'trace-area',     url: baseUrl + 'trace.html'  },
-    { step: 'edit-area',      url: baseUrl + 'edit.html'   },
-    { step: 'define-area',    url: baseUrl + 'define.html' },
-  ];
-
-  var destination = 'qualify.initial'
-
   $stateProvider.state("configure", {
-    url: "",
-    abstract: true,
+    url: "/configure",
     views: {
       'header@': {
-        templateUrl: templateUrl + "header.html", 
-        controller:  "",
+        templateUrl: 'templates/header.html',
+        controller:  'HeaderCtrl as head',
       },
       'main@': {
         templateUrl: baseUrl + "main.html",
-        controllerAs: stateName,
-        controller: function ConfigureCtrl($scope, $state){
-          var currentStep = 0;
-          var vm = this;
-          vm.nextStep = function nextStep(cur){
-            currentStep < steps.length - 1 ? 
-              currentStep++ : $state.go(destination);
-            vm.step=vm.steps[currentStep];
-          };
-          vm.steps = steps;
-          vm.step = vm.steps[currentStep];
-        },
       },
       'footer@': {
         templateUrl: templateUrl + "footer.html",
         controller:  "",
       },
-    },
-  })
-  .state("configure.initial", {
-    url: '/configure',
-    views: {
       'map@configure': {
         templateUrl: baseUrl + "map.html",
         controller:  "",
@@ -57,4 +30,9 @@ angular.module('steps.configure',[]).config( function ($stateProvider) {
       },
     },
   })
+  // .state("configure.initial", {
+  //   url: '/configure',
+  //   views: {
+  //   },
+  // })
 ;});
--- src/js/steps/home.js
+++ src/js/steps/home.js
@@ -5,13 +5,6 @@ angular.module('stage.home',[]).config( function ($stateProvider) {
   var templateUrl = 'templates/';
   var stageUrl = 'templates/stages/' + stageName + '/';
 
-  // var steps = [ 
-  //   { step: 'zip-nearme',   url: stageUrl + 'zip.html'},
-  //   { step: 'address-roof', url: stageUrl + 'address.html'},
-  // ];
-
-  var destination = 'configure.initial'
-
   // state definition
   $stateProvider.state("home", {
     url: "/home",
@@ -22,18 +15,6 @@ angular.module('stage.home',[]).config( function ($stateProvider) {
       },
       'main@': {
         templateUrl: stageUrl + "main.html",
-        // controllerAs: 'stage',
-        // controller:  function HomeCtrl($scope, $state){
-        //   var currentStep = 0;
-        //   var vm = this;
-        //   vm.nextStep = function nextStep(){
-        //     currentStep < steps.length - 1 ? 
-        //       currentStep++ : $state.go(destination);
-        //     vm.step=vm.steps[currentStep];
-        //   };
-        //   vm.steps = steps;
-        //   vm.step = vm.steps[currentStep];
-        // },
       },
       'overlay@home': {
         templateUrl: stageUrl + "overlay.html",
@@ -45,7 +26,6 @@ angular.module('stage.home',[]).config( function ($stateProvider) {
       },
       'footer@': {
         templateUrl: 'templates/footer.html',
-        // controller: 'FooterCtrl as foot',
       },
     },
   })
