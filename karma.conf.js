acee7e6ff6b209257d616183a53e818ec4defb4b
Kurt Werle <kwerle@solarcity.com>
2014-11-13 12:19:38 -0800

    Our first controller test

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function(config) {
       'public/lib/**/*.js',
       'src/js/*.js',
       'src/js/stages/**/*.js',
-      // 'src/js/controllers/*.js',
+      'src/js/controllers/*.js',
       'src/js/services/*.js',
       'test/lib/**/*.js',
       'test/spec/**/*.js'
