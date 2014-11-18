0a195a10f61233181f689852fd164f3ce4e1b627
krry <krry@me.com>
2014-11-17 23:52:25 -0800

    making sure that gulp sees changes to stylesheets, and concats them in the right order

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -11,7 +11,7 @@
 var gulp  = require('gulp')
 
 var paths = {
-  styles:    './src/css/**/*.scss',
+  styles:    ['./src/css/**/*.css','./src/css/**/*.scss'],
   scripts:   './src/js/**/*.js',
   templates: ['./src/templates/**/*.html','./src/index.html'],
 }
