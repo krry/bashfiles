d8089a28cfd5d05b42017636c6bd12e62e6f8cac
krry <krry@me.com>
2014-11-17 23:54:32 -0800

    specking out the stages and states in the userflow

--- src/js/services/StageService.js
+++ src/js/services/StageService.js
@@ -20,6 +20,8 @@ function StageService_ ($state) {
       make a private history function that keeps a record of what you've done
       move config_object to $provider
 
+    TODO: flesh out the stageCtrl, see `docs/stages+states.md`
+
   ================================ */
 
   var StageService = {};
@@ -42,9 +44,21 @@ function StageService_ ($state) {
     {
       name: 'home',
       destination: 'configure',
-      steps: [ 
-        { step: 'zip-nearme',   partial: 'zip.html'     },
-        { step: 'address-roof', partial: 'address.html' },
+      steps: [
+        {
+          step: 'zip-nearme',
+          partial: {
+            primary:   'zip.html',
+            secondary: 'zip-2.html'
+          }
+        },
+        {
+          step: 'address-roof',
+          partial: {
+            primary:   'address.html',
+            secondary: 'address-2.html'
+          }
+        }
       ],
     },
     {
