5188522a5963b0e3dc62a9dd838abd3adfe25aa7
krry <krry@me.com>
2014-11-19 01:36:20 -0800

    groundhog day, groundwork for formControllers, flippant gestures most directions

---
+++ src/js/controllers/FormCtrl.js
@@ -0,0 +1,12 @@
+function FormCtrl_($scope, FormService) {
+  var vm = this;
+
+  $scope.user = FormService.userObj;
+
+  vm.zipCheck = function() {
+    console.log("zipCheck called");
+    
+  }
+}
+
+controllers.controller("FormCtrl", FormCtrl_);
