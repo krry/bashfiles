9f553035d6db102c72fe4deb560d14006bad3909
krry <krry@me.com>
2014-11-18 18:31:22 -0800

    trying to get Jquery listeners working

--- src/js/util.js
+++ src/js/util.js
@@ -17,6 +17,9 @@
   var zipCheck = function(id){
     log("zipCheck got called");
     log('wiring up keyup event listener to element with id ' + id);
+    $('#'+id).focus(function(){
+      log("focused on the " + id);
+    });
     $("#"+id).keyup(function(e){
       log('a key was upped while focused on the "' + id + '" element');
       console.log('effing working');
