df59dea4abec2b67b95d3742a0ca0081ae53b626
krry <krry@me.com>
2014-11-19 01:36:20 -0800

    groundhog day, groundwork for formControllers, flippant gestures most directions

--- src/js/controllers/FormCtrl.js
+++ src/js/controllers/FormCtrl.js
@@ -3,26 +3,9 @@ function FormCtrl_($scope, FormService) {
 
   $scope.user = FormService.userObj;
 
-  vm.checkZip = function(){
+  vm.zipCheck = function() {
     console.log("zipCheck called");
-    // check to see if there are 5 digits
-      // if not, show gentle validation
-      // if so, check if valid US ZIP
-        // if not, show error state and message
-        // if so, pass to checkTerritory API
-          // if not, show out of territory state
-          // if so, advance to address step
-            // return mapService.initNearMe()
-  };
-
-  vm.parseAddress = function(){
-    console.log('parsing address');
-    // package and send full address to Google Maps API for sanitation
-    // populate sanitized address in the address fields
-    // center map on latlng of address
-    // check if street address present in sanitized address
-      // if so, drop map marker on this location
   }
-};
+}
 
 controllers.controller("FormCtrl", FormCtrl_);
--- src/js/util.js
+++ src/js/util.js
@@ -19,10 +19,9 @@
   var zipCheck = function(id){
     log("zipCheck got called");
     log('wiring up keyup event listener to element with id ' + id);
-    $('#'+id).focus(function(){
-      log("focused on the " + id);
+    $('#'+id).on("focus", function(){
     });
-    $("#"+id).keyup(function(e){
+    $("#"+id).on("keyup", function(e){
       log('a key was upped while focused on the "' + id + '" element');
       console.log('effing working');
       var $zipVal = $(this).val();
