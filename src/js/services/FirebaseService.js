31d8388ce453f5ce1a017ddaa8fcdc1d1e5dc292
krry <krry@me.com>
2014-11-20 14:07:01 -0800

    trying to fix Firebase on this userflow branch

--- src/js/services/FirebaseService.js
+++ src/js/services/FirebaseService.js
@@ -1,44 +1,54 @@
 angular.module('flannel.firebase', [])
 // a simple utility to create references to Firebase paths
-  .factory('firebaseRef', ['$firebase', 'FBURL', function ($firebase, FBURL) {
-    /**
-     * @function
-     * @name firebaseRef
-     * @param {String|Array...} path
-     * @return a Firebase instance
-     */
-     return function(path) {
-      return new Firebase(pathRef([FBURL].concat(Array.prototype.slice.call(arguments))));
-    };
-  }])
-  // a factory for sync'd geometry strings
-  .factory('syncGeometry', ['$firebase', 'FBURL', function ($firebase, url) {
-
-    function syncGeometry (geometry) { // TODO: get design ID
-      // create a reference to the geometry
-      var ref = new Firebase(url + '/designId/geometries'); 
-      // return it as a synchronized object
-      return $firebase(ref).$asObject();
-    }
-
-    return syncGeometry;
-  }])
-
-  // a simple utility to create $firebase objects from angularFire
-  .service('syncData', ['$firebase', 'firebaseRef', function($firebase, firebaseRef) {
-      /**
-       * @function
-       * @name syncData
-       * @param {String|Array...} path
-       * @param {int} [limit]
-       * @return a Firebase instance
-       */
-       return function syncData(path, limit) {
-         var ref = firebaseRef(path);
-         // limit && (ref = ref.limit(limit));
-         return $firebase(ref);
-       };
-     }]);
+.factory('firebaseRef', ['$firebase', 'FBURL', function ($firebase, FBURL) {
+  /**
+   * @function
+   * @name firebaseRef
+   * @param {String|Array...} path
+   * @return a Firebase instance
+   */
+  return function(path) {
+    return new Firebase(pathRef([FBURL].concat(Array.prototype.slice.call(arguments))));
+  }
+}])
+// a simple utility to create $firebase objects from angularFire
+.service('syncData', ['$firebase', 'firebaseRef', function ($firebase, firebaseRef) {
+  /**
+   * @function
+   * @name syncData
+   * @param {String|Array...} path
+   * @param {int} [limit]
+   * @return a Firebase instance
+   */
+  return function syncData(path, limit) {
+    var ref = firebaseRef(path);
+    limit && (ref = ref.limit(limit));
+    return $firebase(ref);
+  }
+}])
+.service('updateArea', ['$firebase', 'firebaseRef', function ($firebase) {
+  /**
+   * @function
+   * @name setWkt
+   * @param {String}
+   * @return a Firebase Obj
+   */
+  return function updateArea (ref, newVal) {
+    var val = ref.update(newval);
+    return val;
+  }
+}]).service('addWkt', ['$firebase', 'firebaseRef', function ($firebase) {
+  /**
+   * @function
+   * @name setWkt
+   * @param {String}
+   * @return a Firebase Obj
+   */
+  return function addWkt (design_areas_ref, wkt_txt) {
+    var wkt_ref = design_areas_ref.push(wkt_txt);
+    return wkt_ref;
+  }
+}]);
 
 function pathRef(args) {
   for(var i=0; i < args.length; i++) {
