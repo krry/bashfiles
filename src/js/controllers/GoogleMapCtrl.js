023956b2c3e0c6fe9e2081ddb2620d3c38d80e76
Kerry Snyder <krry@me.com>
2014-12-16 15:58:56 -0800

    fixing google maps, you can now enter a zip code and the map goes there

--- src/js/controllers/GoogleMapCtrl.js
+++ src/js/controllers/GoogleMapCtrl.js
@@ -3,6 +3,8 @@ controllers.controller("GoogleMapCtrl", ["$scope", "$element", "MapService", "Us
 function GoogleMapCtrl_($scope, $element, MapService, UserService) {
   var vm = this;
 
+  vm.gmapShown = MapService.getGmapShown;
+
   var mapOptions,
       map,
       input,
@@ -11,21 +13,14 @@ function GoogleMapCtrl_($scope, $element, MapService, UserService) {
   mapOptions = MapService.g.mapOptions;
   map = MapService.setGmap($element[0], mapOptions);
 
-  setTimeout(function(){
-    activate();
-  }, 500);
-
-  // $scope.$on('mapUpdate', function(event, location) {
-  //   console.log(event);
-  //   MapService.updateGmap(location);
-  // });
+  google.maps.event.addDomListener(window, "load", activate);
 
   function activate(){
     // create an Autocompleting search box on the map
     input = document.getElementById('hood_check');
     // map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(input);
     searchbox = MapService.setGmapSearchBox(input);
-    searchbox.bindTo('bounds', map);
+    // searchbox.bindTo('bounds', map);
     // listen for the 'place_changed' trigger which is fired
     google.maps.event.addListener(searchbox, 'place_changed', parsePlace);
     MapService.updateGmap(MapService.getGmapCenter());
@@ -37,7 +32,7 @@ function GoogleMapCtrl_($scope, $element, MapService, UserService) {
     if (!place.geometry) {
       return;
     }
-    // If the place has a geometry then present it on the map.
+    // If the place has a geometry, then present it on the map.
     if (place.geometry.viewport) {
       map.fitBounds(place.geometry.viewport);
     } else {
@@ -62,7 +57,9 @@ function GoogleMapCtrl_($scope, $element, MapService, UserService) {
   function saveCenter () {
     var center = map.getCenter();
     if (center) {
-      MapService.setCenter(center);
+      // this is what was blowing everything up, don't know exactly why
+      // MapService.setCenter(center);
+      MapService.setGmapCenter(center);
     }
   }
 
