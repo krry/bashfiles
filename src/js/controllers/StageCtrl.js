79d5bdee40a2a01010008da025102c98deee1b7d
Jesse LaRue <lazaruslarue@gmail.com>
2014-12-23 15:21:18 -0800

    stage controller updates partials based on serverstream

--- src/js/controllers/StageCtrl.js
+++ src/js/controllers/StageCtrl.js
@@ -1,23 +1,51 @@
-controllers.controller("StageCtrl", ["$scope", "$state", "StageService", "InteractionService", "LayerService", "SyncService", "JwtService", "syncData", StageCtrl_]);
+controllers.controller("StageCtrl", ["$scope", "$state", "StageService", "InteractionService", "LayerService", "SyncService", "JwtService", "syncData", "StreamService", "Session", StageCtrl_]);
 
-function StageCtrl_($scope, $state, StageService, InteractionService, LayerService, SyncService, JwtService, syncData) {
+function StageCtrl_($scope, $state, StageService, InteractionService, LayerService, SyncService, JwtService, syncData, stream, Session) {
   // This controller should be used for anything that needs to control which partials are being used
   var vm = this;
   var config = StageService.config;
   $scope.sync  = StageService.syncObj;
   // stage & step index numbers
-  var stage = $scope.sync().stage;
-  var step  = $scope.sync().step;
 
   // init
   JwtService.jwt();
   vm.partials = StageService.partials;
   vm.partial = vm.partials[0][0];
 
-  function updateUi () {
-    stage = $scope.sync().stage;
-    step  = $scope.sync().step;
-  }
+// /////////////////////////////////////////////////
+  // set initial values for step and stage:
+  var stage = 0;
+  var step  = 0;
+
+  var session_ref = SyncService.get('session_ref');
+
+  $scope.partial_sync = true;
+
+  var session_stream = Session.subscribe(function handle_session_stream (data) {
+    var session = data.val();
+    console.log('session in session stream subscription: ', session);
+    if (session.stage !== stage) {
+      $scope.partial_sync = false;
+      // update the stage on the client
+      // go to new state
+        // TODO: figure out state name
+        // TODO: refactor state to accept step&stage params
+        // $state.go(state_name, session);
+    } else if (session.step !== step) {
+      $scope.partial_sync = false;
+      console.log('step was updated by server: ', session);
+      // update the step
+      step = session.step;
+    }
+    // after making changes, sync with the service.
+      // send 'updateview' event
+
+    if (!$scope.session_synchronized) {
+      console.log('calling sync: *************** ');
+      syncWithService();
+    }
+  });
+
 
   function syncWithService() {
     updateUi();
@@ -25,13 +53,25 @@ function StageCtrl_($scope, $state, StageService, InteractionService, LayerServi
     updatePartials(stage, step);
   }
 
+  function updateUi () {
+    stage = $scope.sync().stage;
+    // step  = $scope.sync().step;
+  }
+
   function updatePartials (stg, stp) {
-    // vm.partials = StageService.partials[stg];
-    console.log('should update partials', arguments)
+    console.log('updating partials', stg, stp);
     vm.partial = StageService.partials[stg][stp];
+    $scope.partial_sync = true;
     $scope.$apply();
   }
 
+  var client_stream = new stream()
+  client_stream.listen('change', function handle_server_diff (data) {
+    console.log(' (((((((((((((((((((((((((( handle server step, data: ', data);
+    session_ref.update(data);
+  });
+
+// /////////////////////////////////////////////////
   vm.next = function(){
     $scope.sync().next();
     syncWithService();
@@ -59,20 +99,20 @@ function StageCtrl_($scope, $state, StageService, InteractionService, LayerServi
   //   return parts;
   // }
 
-  $scope.$watch(
-    function(){ return $scope.sync().stage; },
-    function(newVal, oldVal){
-    if (newVal !== oldVal){
-      updatePartials($scope.sync().stage, $scope.sync().step);
-    }
-  });
-  $scope.$watch(
-    function(){ return $scope.sync().step; },
-    function(newVal, oldVal){
-    if (newVal !== oldVal){
-      updatePartials($scope.sync().stage, $scope.sync().step);
-    }
-  });
+  // $scope.$watch(
+  //   function(){ return $scope.sync().stage; },
+  //   function(newVal, oldVal){
+  //   if (newVal !== oldVal){
+  //     updatePartials($scope.sync().stage, $scope.sync().step);
+  //   }
+  // });
+  // $scope.$watch(
+  //   function(){ return $scope.sync().step; },
+  //   function(newVal, oldVal){
+  //   if (newVal !== oldVal){
+  //     updatePartials($scope.sync().stage, $scope.sync().step);
+  //   }
+  // });
 
   /// dev code ///
   vm.areaone = function () {
