ccfc55a83c66daa2335df2f392aa8fc00ea5ceab
krry <krry@me.com>
2014-12-22 20:31:12 -0800

    working on link functions

--- src/js/directives/flnModal.js
+++ src/js/directives/flnModal.js
@@ -5,5 +5,17 @@ function flnModal () {
     templateUrl: 'templates/directives/flnModal.html',
     controller: 'ModalCtrl',
     controllerAs: 'modal',
+    link: function ModalLink(scope, element, attr, ctrl) {
+      $('.modal-trigger').on('click', function(){
+        debugger;
+        if (ctrl.isOn()) {
+          console.log('stopping body scroll');
+          $('body').css('overflow', 'hidden');
+        } else {
+          console.log('reallowing body scroll');
+          $('body').css('overflow', 'scroll');
+        }
+      });
+    }
   };
 }
