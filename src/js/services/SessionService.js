faf70994a0b4b6d4b9a8fac215986f6167cf0dbf
Jesse LaRue <lazaruslarue@gmail.com>
2014-12-23 15:21:55 -0800

    new services to a) handle session & b) stream factory

---
+++ src/js/services/SessionService.js
@@ -0,0 +1,42 @@
+providers.provider('Session', SessionProvider_);
+function SessionProvider_ () {
+  /* ================================
+    Session
+      structures the user flow, e.g.:
+
+    sessions: {
+      session_id: {
+        state: {
+          stage: 0,
+          step:  0,
+        },
+        event_log:  [{event},{event},{event},...],
+        agent:      "agent_id",
+        prospect:   "prospect_id",
+        design:     "design_id",
+        start_time: "timetimetime",
+        end_time:   "timetimetime",
+      }
+    }
+
+    TODO:
+      make a private history function that keeps a record of what you've done
+
+  ================================ */
+
+  var session_ref = new Firebase('https://scty.firebaseio.com/states/1234/state');
+  console.log("session_ref", session_ref)
+  var fb_observable = session_ref.observe('value');
+
+  this.$get = [ "JwtService", "SyncService", function SessionProviderFactory(jwt, sync) { // TODO: provide auth object to this
+    console.log('Session Provider Loads Here: *************************************************** ',arguments);
+    // TODO: pass arguments to this $get method to change the fb_observable's_ref
+    console.log('Provides: ', fb_observable);
+    sync.set('session_ref', session_ref)
+    console.l
+    return fb_observable
+  } ]
+
+  this.session_stream = fb_observable;
+  this.ref = session_ref;
+}
