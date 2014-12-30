3001eec6204d14bd65c0a0edc24c1eb3cf14b4d6
Jesse LaRue <lazaruslarue@gmail.com>
2014-12-30 14:25:48 -0800

    fixup! provide new "staging" task for gulp on Jenkins build machine. add config.js to .gitignore

--- gulp/tasks/staging.js
+++ gulp/tasks/staging.js
@@ -2,5 +2,5 @@ var gulp = require('gulp')
 var runSeq = require('run-sequence').use(gulp)
 
 gulp.task('staging',  function(){
-  runSeq('config', 'build');
+  runSeq('config', 'build', 'demon');
 })
