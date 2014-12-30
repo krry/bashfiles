2ae19ea7000edb78ad32f7b11d5d59401d049965
Jesse LaRue <lazaruslarue@gmail.com>
2014-12-29 21:35:48 -0800

    remove un-needed code in homestate

--- src/js/stages/home.js
+++ src/js/stages/home.js
@@ -18,15 +18,6 @@ angular.module('stages.home',[]).config( function ($stateProvider) {
       'main@': {
         resolve: {},
         templateUrl: stageUrl + "main.html",
-        controllerAs: "home",
-        controller:  function home_ctrl ($scope, SyncService, firebaseRef) {
-          // the design_ref may be set by arriving from a share link, or should be generated new
-          $scope.designRef = SyncService.get('design_ref') || firebaseRef('/designs').push()
-          .once( 'value', function (dataSnapshot) {
-            SyncService.set('design_ref', dataSnapshot);
-            console.log('your design id is new: ', dataSnapshot.key());
-          });
-        },
       },
       'overlay@home': {
         templateUrl: stageUrl + "overlay.html",
