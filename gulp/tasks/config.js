d02d90b2cec8aa50aaf702c6375fa70ad5a87bda
Kerry Snyder <krry@me.com>
2014-12-31 19:57:58 -0800

    mostly fixed the config build issue, still have to start the app twice to make sure

--- gulp/tasks/config.js
+++ gulp/tasks/config.js
@@ -1,9 +1,10 @@
 /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
   CONFIG
+  depends on CLEAN task
   supports DEV, STAGING, and PROD tasks
 
-  gulp pulls in environment-specific variables via JSON, 
+  gulp pulls in environment-specific variables via JSON,
   then writes it to a config.js file which is then
   concatenated with the rest of the scripts and served
   into `index.html`
@@ -14,6 +15,7 @@
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 
 var gulp = require('gulp');
+var del = require('del')
 var rename = require('gulp-rename');
 var ngConstant = require('gulp-ng-constant');
 
@@ -27,7 +29,10 @@ var config = {
 };
 
 var confile = './server/config/environments/'+env+'.json';
-gulp.task('config', function () {
+
+gulp.task('config', ['clean'], function () {
+  console.log("[config  ] wiping 'config.js'");
+  del('./src/js/config.js');
   console.log("[config  ] using:" , confile, "as environment config file");
   gulp.src(confile)
   .pipe(ngConstant(config))
--- gulp/tasks/dev.js
+++ gulp/tasks/dev.js
@@ -2,15 +2,32 @@
 
   DEV
   root task
+  depends on CONFIG task
 
   cleans out the compiled files and makes sure that gulp
   is watching itself for changes as well
 
+  gulp watches changes in itself, reboots itself
+
+  slightly adapted from noxoc's solution
+  http://noxoc.de/2014/06/25/reload-gulpfile-js-on-change/
+
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 
-var gulp = require('gulp');
-var runSeq = require('run-sequence');
+var gulp = require('gulp')
+var spawn = require('child_process').spawn;
 
-gulp.task('dev', function(){
-  runSeq('clean', 'config', 'meta');
-})
+gulp.task('dev', ['clean'], function() {
+  var process;
+
+  function restart() {
+    if (process) {
+      process.kill();
+    }
+
+    process = spawn('gulp', ['default'], {stdio: 'inherit'});
+  }
+
+  gulp.watch(['gulp/tasks/**/*.js', 'package.json'], restart);
+  restart();
+});
