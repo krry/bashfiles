c43f0d49ca2e1351ddea49e74875470632574a8f
Kerry Snyder <krry@me.com>
2014-12-31 22:42:35 -0800

    gulp sass errors handled better

--- gulp/util/handleErrors.js
+++ gulp/util/handleErrors.js
@@ -4,9 +4,10 @@ module.exports = function() {
 
   var args = Array.prototype.slice.call(arguments);
   // Send error to notification center with gulp-notify
+  console.error(args);
   notify.onError({
     title: "<%= error.title %>",
-    message: "<%= error.message %>"
+    message: "<%= error.message %>",
   }).apply(this, args);
 
   // Keep gulp from hanging on this task
