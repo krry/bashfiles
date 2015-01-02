d41b73ef366024e352dff16c4eb8842344a9678e
Jesse LaRue <lazaruslarue@gmail.com>
2015-01-02 07:14:36 -0800

    the worst commit: part deux

--- test/spec/services/Stage.js
+++ test/spec/services/Stage.js
@@ -1,18 +1,18 @@
-// describe('Unit: StageService', function() {
+describe('Unit: StageService', function() {
 
-//   beforeEach(module('flannel'));
+  // beforeEach(module('flannel'));
 
-//   var ctrl, scope;
+  // var ctrl, scope;
 
-//   beforeEach(inject(function (_StageService_, $state) {
-//     stageService = _StageService_;
-//     state = $state;
-//   }));
+  // beforeEach(inject(function (_StageService_, $state) {
+  //   stageService = _StageService_;
+  //   state = $state;
+  // }));
 
-//   it("StageService' .next() should initially return 0.",
-//     function() {
-//       // note, this test fails if there's only one step in a stage
-//       // expect(stageService.syncObj().next()).toEqual(0);
-//   });
+  it("StageService' .next() should initially return 0.",
+    function() {
+      // note, this test fails if there's only one step in a stage
+      // expect(stageService.syncObj().next()).toEqual(0);
+  });
 
-// })
+})
