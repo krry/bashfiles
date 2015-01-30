525f859e2b1d86718d681de1c73f91d1354ed7d0
Jesse LaRue <lazaruslarue@gmail.com>
2015-01-29 17:19:24 -0800

    app looks for $cookies.uid & updates the User accordingly

--- src/js/app.js
+++ src/js/app.js
@@ -23,11 +23,16 @@ angular.module('flannel', [
   $sceProvider.enabled(false);
   $httpProvider.defaults.useXDomain = true;
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}).run(function() {
-
+}).run(function($cookies, User) {
+  // pull the user id from an existing cookie
+  var uid = $cookies.uuid
+  typeof uid !== "undefined" && (uid = uid.split(":")[1].split(".")[0]) // hack: is this too ugly to live?
+  // if there was an existing cookie, then we'll grab the existing user.
+  uid && User.ref(uid); // todo: could this be authUserAndUpdateRef instead?
 });
 
 var providers   = angular.module('flannel.providers',[]);
 var controllers = angular.module('flannel.controllers',[]);
 var directives  = angular.module('flannel.directives',[]);
 var options     = angular.module('flannel.options',[]);
+
