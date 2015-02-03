cf53a38d03db213b87582e3f1c69357fe83fa44b
Jesse LaRue <lazaruslarue@gmail.com>
2015-02-03 13:44:23 -0800

    hack: dev: for token fix

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -19,7 +19,8 @@ module.exports = function(app) {
     if (uuid !== null && uuid !== undefined) {
       // Look up the document by uuid and return that one
     } else {
-      uuid = UUID.v1();
+      // uuid = UUID.v1();
+      uuid = '25B4EE58-ABED-11E4-9E06-C7692BCBFA65' // HACK: DEV: overriding to fix Invalid Token errors.
       res.cookie('uuid', uuid, { maxAge: 1*365*24*60*60*1000, signed: true });
       res.cookie('edit', 1, { maxAge: 1*365*24*60*60*1000, signed: true });
     }
@@ -33,13 +34,15 @@ module.exports = function(app) {
     var uuid, edit, _jwt;
 
     // console.log(req.signedCookies);
-    uuid = req.signedCookies.uuid;
+    // uuid = req.signedCookies.uuid; // HACK: DEV: overriding to fix Invalid Token errors.
+    uuid = '25B4EE58-ABED-11E4-9E06-C7692BCBFA65' // HACK: DEV: overriding to fix Invalid Token errors.
     edit = req.signedCookies.edit;
     if (uuid !== null && uuid !== undefined) {
       // console.log("About to set uuid: " + uuid + ".");
       _jwt = generateJwt(uuid, edit);
     } else {
       _jwt = 'you lying so and so';
+
     }
     // now we need to pass the _jwt to the client
     res.send(_jwt);
