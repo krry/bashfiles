b47885a8989c08eb8a0467fddc78903c44b39ede
Jesse LaRue <lazaruslarue@gmail.com>
2015-02-04 19:23:57 -0800

    setting routes for flannel

--- server/routes/appRoutes.js
+++ server/routes/appRoutes.js
@@ -2,7 +2,6 @@
 module.exports = function(app) {
   var appController = require('../controllers/appController.js')(app);
 
-  app.get('/', appController.index );
-  app.get('/token_fix', appController.index ); // HACK: DEV: overriding to fix Invalid Token errors.
+  app.get('/flannel/', appController.index );
   app.get('/jwt', appController.jwt );
 };
