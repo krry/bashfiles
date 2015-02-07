6058f18d743b7f3f4cce8db1fc1f9059b0fb30da
Jesse LaRue <lazaruslarue@gmail.com>
2015-02-06 20:06:42 -0800

    fixup! remove url portions from application states

--- src/js/stages.js
+++ src/js/stages.js
@@ -6,7 +6,8 @@ angular.module('stages',[
   'signup',
 ])
 .config(["$locationProvider", "$stateProvider", "$urlRouterProvider", function ($locationProvider, $stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
+  $locationProvider.hashPrefix(''); // remove angular's # and let $location.hash make more sense
+  $urlRouterProvider.otherwise('/'); // if users arrive somewhere other than the root URL, send them to the root.
 }]).run([function ui_router_run() {
   // this runs after all the dependencies are bootstrapped
 }]);
