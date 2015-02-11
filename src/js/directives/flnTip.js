460ea1e0f92a4e1543852e7a7458994b624be09a
krry <krry@me.com>
2015-02-10 20:57:58 -0800

    polishing the tooltips

---
+++ src/js/directives/flnTip.js
@@ -0,0 +1,25 @@
+directives.directive('flnTip', [flnTip_]);
+
+function flnTip_ () {
+  return {
+    restrict: "E",
+    transclude: true,
+    scope: {
+      tiptext: "@tiptext",
+      position: "@position",
+    },
+    templateUrl: "templates/directives/flnTip.html",
+    controller: function ($scope) {
+      console.log('tip control', $scope);
+    },
+    link: function (scope, element, attrs) {
+      console.log('tooling tip', scope);
+      $(element).on('hover', function () {
+        scope.tipShown = true;
+      });
+      $(element).on('blur', function () {
+        scope.tipShown = false;
+      });
+    },
+  }
+}
