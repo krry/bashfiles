ccce7994e530cbada851a9c8ed82acd13324cb00
Kerry Snyder <krry@me.com>
2015-02-16 16:20:16 -0800

    resolving conflicts from rebasing on develop'

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@ angular.module('flannel', [
   'flannel.controllers',
   'flannel.directives',
   'nouislider'
-]).config(['$sceDelegateProvider', '$sceProvider', '$httpProvider', 'UserProvider', function($sceDelegateProvider, $sceProvider, $httpProvider, UserProvider) {
+]).config(['$sceDelegateProvider', '$sceProvider', '$httpProvider', 'UserProvider' function($sceDelegateProvider, $sceProvider, $httpProvider, UserProvider) {
   // hack: sorta hacky... but maybe not.
   // http://stackoverflow.com/questions/20588114/how-to-use-cookiesprovider-in-angular-config
   var $cookies, uid;
@@ -46,7 +46,17 @@ angular.module('flannel', [
   $sceProvider.enabled(false);
   $httpProvider.defaults.useXDomain = true;
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}]).run(["User", "Clientstream", function run_app(User, Client) {
+
+  // mailgun options for email validation service
+  var mailgunOptions = {
+    api_key: 'pubkey-38f603eccf9df24d29de2c548c3d5a55',
+    in_progress: null,
+    success: null,
+    error: null,
+  };
+  MailgunProvider.configure(mailgunOptions);
+
+}).run(["User", "Clientstream", function run_app(User, Client) {
   // let the app know we've gotten the important stuff :)
   User.ref().once('value', function setSessionFromUser (ds) {
     // let session know we've loaded the User. now load the session.
