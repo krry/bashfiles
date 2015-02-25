3e92cb85d04d483b7dbc99337bbc230345f8fa57
Kerry Snyder <krry@me.com>
2015-02-24 21:55:26 -0800

    reset the form on the front and back ends in one click

--- src/js/providers/FormProvider.js
+++ src/js/providers/FormProvider.js
@@ -51,18 +51,18 @@ function FormProvider_ () {
 
   this.$get = ["Clientstream",function formProviderFactory(Client) {
 
-
     Client.listen('Session: Loaded', bootstrapForm);
     Client.listen('valid zip', updateZipOnRef);// TODO: fix line use Client.listen('valid format', updateRefByKey ); or something
     Client.listen('Form: valid house', updateRefByVal );
     Client.listen('Form: valid data', updateRefByVal);
 
     // DEV:
-    Client.listen('Dev: Reset form', restartForm);
+    Client.listen('Dev: Reset form', resetForm);
 
-    function restartForm () {
+    function resetForm () {
       var form_obj = {bill: 100};
       _ref.set(form_obj);
+      _ref.once('value', processNewFormFromFirebase );
     }
     // DEV: end
 
