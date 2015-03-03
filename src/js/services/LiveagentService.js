54614a4ad9f018ec24632eb81ee05a6b4fed2ac4
Jesse LaRue <lazaruslarue@gmail.com>
2015-03-03 15:20:12 -0800

    send session_id in different way for salesforce

--- src/js/services/LiveagentService.js
+++ src/js/services/LiveagentService.js
@@ -37,6 +37,13 @@ function LiveagentService_ () {
               true,         // doFind
               true,         // isExactMatch
               true)         // doCreate
+              .map(
+              "Session_ID__c",
+              "session_id",
+              false,
+              false,
+              true
+              )
       .showOnCreate().saveToTranscript("ODA_Session__c");
 
     // initialize the liveagent session
@@ -89,4 +96,3 @@ function LiveagentService_ () {
     }
   }
 }
-
