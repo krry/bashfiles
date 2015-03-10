ae9cf00e766f797ea2d5dba468e37f289279952b
Jesse LaRue <lazaruslarue@gmail.com>
2015-03-10 08:06:27 -0700

    Layer factory with listeners available to listen to things with

--- src/js/services/Configurator/Layers.js
+++ src/js/services/Configurator/Layers.js
@@ -8,18 +8,41 @@
 
 angular.module('flannel').factory('Layers', ['Design', 'StyleService', Layers_]);
 
-function Layers_(Design, Style) {
+function Layers_(Design, Styles) {
 
-    var layers, vector, source;
+  var layers, l_draw, source;
 
-    source = Design.area_source;
+  draw_source = Design.draw_source;
+  modify_source = Design.modify_source;
 
-    l_vector = new ol.layer.Vector({source: source});
-    layers = [ l_vector ];
+  l_draw = new ol.layer.Vector({
+    source: draw_source,
+    style:  Styles.defaultStyleFunction,
+  });
+  l_modify = new ol.layer.Vector({
+    source: modify_source,
+    style:  Styles.highlightStyleFunction,
+  });
+  layers = [ l_draw, l_modify ];
+  /* draw_source listeners */
+  draw_source.on('addfeature',function(e){
+    // console.debug('draw_source: addfeature',e);
+    // console.debug('featuresremaining = ', draw_source.getFeatures().length);
+  });
+  draw_source.on('removefeature',function(e){
+    // console.debug('draw_source: removefeature',e);
+    // console.debug('featuresremaining = ', draw_source.getFeatures().length);
+  });
 
-    source.on('addfeature',function(e){
-      console.debug('source: addfeature',e);
-    });
+  /* modify_source listeners */
+  modify_source.on('addfeature',function(e){
+    // console.debug('modify_source: addfeature',e);
+    // console.debug('featuresremaining = ', modify_source.getFeatures().length);
+  });
+  modify_source.on('removefeature',function(e){
+    // console.debug('modify_source: removefeature',e);
+    // console.debug('featuresremaining = ', modify_source.getFeatures().length);
+  });
 
   return layers;
 }
