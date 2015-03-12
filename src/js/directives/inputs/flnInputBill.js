d7e9744488af7c68bc8222c3fd7bee42883680dd
Kerry Snyder <krry@me.com>
2015-03-12 13:10:51 -0700

    updating and saving bill on slider change

--- src/js/directives/inputs/flnInputBill.js
+++ src/js/directives/inputs/flnInputBill.js
@@ -1,15 +1,20 @@
-directives.directive('flnInputBill', ["defaultValues", "Form", flnInputBill_]);
+directives.directive('flnInputBill', ['defaultValues', 'Form', flnInputBill_]);
 
 function flnInputBill_ (defaultValues, Form) {
   return {
     restrict: "A",
     templateUrl: "templates/directives/inputs/flnInputBill.html",
     require: "^flnForm",
-    link: function (scope, element, attrs) {
-      var prospect = Form.prospect
+    link: function (scope, element, attrs, FormCtrl) {
+      var prospect = Form.prospect;
+
       if (!prospect.bill) {
         prospect.bill = defaultValues.bill;
       }
+
+      element.find('input').bind('change', function () {
+        FormCtrl.saveBill(prospect.bill);
+      })
     }
   };
 }
