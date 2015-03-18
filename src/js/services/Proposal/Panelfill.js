3af77ab59e32a474c273a257fbfb33eda6574350
Jesse LaRue <lazaruslarue@gmail.com>
2015-03-18 06:33:50 -0700

    panelfill works!

--- src/js/services/Proposal/Panelfill.js
+++ src/js/services/Proposal/Panelfill.js
@@ -14,15 +14,12 @@ function PanelfillSvc ($http, $q) {
       area_message,
       msg;
 
-
   Panelfill.getFilled = function(wkt_txt) {
     var deferred = $q.defer();
     var x = {};
 
     // turn text into polygon message
-    // wkt_txt = 'POLYGON((-122.39737428229726 37.75635781957059,-122.39735416572965 37.75610971523671,-122.39714093011297 37.75611910296826,-122.39715866974932 37.756259541756016,-122.39717311662115 37.75637391282468,-122.39737428229726 37.75635781957059))';
-    debugger;
-    points_for_panelfill = fillMessageForSingleArea(wkt_txt)['m'][0]; //HACK: reused code
+    points_for_panelfill = fillMessageForSingleArea(wkt_txt)['m'][0]; //HACK: reused code, ignore obstructions
     function fillMessageForSingleArea(wkt_txt){
       var result = {};
       // add area points object
@@ -64,124 +61,12 @@ function PanelfillSvc ($http, $q) {
       success: function(dt){
         var t = JSON.parse(dt);
         panelfill_points = t[0]; // HACK: ignoring setbacks that come with this message
-        // array_of_features = makePanelsWithApiResponse(panelfill_points);
-        console.debug('successful panelfill response', panelfill_points);
         deferred.resolve(panelfill_points);
       }
     });
-
-
-
-    //
-    // function makePanelsWithApiResponse(data){
-    //   var featurestoadd = [];
-    //   var responseIterator = function(points){
-    //     // iterate over each panel in the array of panels
-    //     var feature = panelFromJson(points);
-    //     featurestoadd.push(feature);
-    //   };
-    //   angular.forEach(data, responseIterator);
-    //   return featurestoadd;
-    // };
-
-
-    // function panelFromJson(array_of_points){
-    //   var feature_to_return;
-    //   var pts = array_of_points;
-    //
-    //   function pointJoin(pointCoordLngLat){
-    //     return pointCoordLngLat.join(' ');
-    //   }
-    //   // create a string with the proper start and end
-    //   var wkt_string_from_points_array = [
-    //       pointJoin(pts[0]),
-    //       pointJoin(pts[1]),
-    //       pointJoin(pts[2]),
-    //       pointJoin(pts[3]),
-    //       pointJoin(pts[0]),
-    //   ].join(', ');
-    //
-    //   // add the right begin & end details
-    //   wkt_string_from_points_array = "POLYGON((" + wkt_string_from_points_array + "))";
-    //
-    //   feature_to_return = wkt.readFeature(wkt_string_from_points_array);
-    //   var fGeometry     = wkt.readGeometry(wkt_string_from_points_array);
-    //
-    //   feature_to_return.set('panel', fGeometry)
-    //   feature_to_return.setGeometryName('panel');
-    //   return feature_to_return;
-    // };
-
 
     return deferred.promise;
   };
 
   return Panelfill;
 }
-
-
-/*
-    x = JSON.stringify(msg);
-    $.ajax({
-      type: "POST",
-      url: baseUrl,
-      data: {
-         "TestJSON": x
-      },
-      success: function(dt){
-        var t = JSON.parse(dt);
-        panelfill_points = t[0]; // HACK: ignoring setbacks that come with this message
-        array_of_features = makePanelsWithApiResponse(panelfill_points);
-        deferred.resolve(array_of_features);
-      }
-    });
-
-
-
-
-    function makePanelsWithApiResponse(data){
-      var featurestoadd = [];
-      var responseIterator = function(points){
-        // iterate over each panel in the array of panels
-        var feature = panelFromJson(points);
-        featurestoadd.push(feature);
-      };
-      angular.forEach(data, responseIterator);
-      return featurestoadd;
-    };
-
-
-    function panelFromJson(array_of_points){
-      var feature_to_return;
-      var pts = array_of_points;
-
-      function pointJoin(pointCoordLngLat){
-        return pointCoordLngLat.join(' ');
-      }
-      // create a string with the proper start and end
-      var wkt_string_from_points_array = [
-          pointJoin(pts[0]),
-          pointJoin(pts[1]),
-          pointJoin(pts[2]),
-          pointJoin(pts[3]),
-          pointJoin(pts[0]),
-      ].join(', ');
-
-      // add the right begin & end details
-      wkt_string_from_points_array = "POLYGON((" + wkt_string_from_points_array + "))";
-
-      feature_to_return = wkt.readFeature(wkt_string_from_points_array);
-      var fGeometry     = wkt.readGeometry(wkt_string_from_points_array);
-
-      feature_to_return.set('panel', fGeometry)
-      feature_to_return.setGeometryName('panel');
-      return feature_to_return;
-    };
-
-
-    return deferred.promise;
-  };
-
-  return Panelfill;
-
-*/
--- src/js/services/Proposal/Proposal.js
+++ src/js/services/Proposal/Proposal.js
@@ -12,8 +12,8 @@ function Proposal_(Design, Panelfill, Client) {
   var map_options = {
     zoom: 20,
     mapTypeId: google.maps.MapTypeId.SATELLITE,
-    disableDoubleClickZoom: true,
-    scrollwheel: true,
+    disableDoubleClickZoom: false,
+    scrollwheel: false,
     streetViewControl: false,
     disableDefaultUI: true,
     keyboardShortcuts: false,
@@ -69,7 +69,6 @@ function Proposal_(Design, Panelfill, Client) {
       function compareAgainstMax(pt){
         bounds.extend(pt);
       }
-      debugger;
       map.setCenter(bounds.getCenter());
       p_array.forEach(function(polygon){
         polygon.setMap(map);
@@ -77,104 +76,3 @@ function Proposal_(Design, Panelfill, Client) {
     })
   }
 }
-
-
-/*
-  var gmap,
-      omap,
-      // defaults
-      gmap_options,
-      omap_options,
-      omap_controls,
-      // follow this to make sure we're converting properly
-      gmap_projection;
-
-  gmap_options = {
-    disableDefaultUI: true,
-    keyboardShortcuts: false,
-    draggable: false,
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    disableDoubleClickZoom: true,
-    scrollwheel: false,
-    streetViewControl: false
-  };
-
-  omap_controls = [];
-
-
-
-  omap_options = {
-    layers: [],
-    controls:  [],
-    interactions: [],
-  }
-
-  // startup the map inside the DOM
-  this.setTarget = setTargetOfMaps;
-
-  // maps (defined by #setTargetOfMaps);
-  this.map = {
-    omap: omap,
-    gmap: gmap,
-  };
-
-
-  var area_ref = new Firebase('https://scty.firebaseio.com/designs/butts/areas/0')
-
-  area_ref.once('value', function (ds) {
-    area_wkt = ds.exportVal().wkt;
-    Panelfill.getFilled(area_wkt).then(function handlePanelfillFeatures(f_array) {
-      // chain calls to fulfill
-
-      // style feature array
-      // add feature array to source
-      // get extent of source
-      // set gmap extent with source extent
-      // set omap extent with source extent
-    })
-  });
-
-
-
-
-
-  function setTargetOfMaps(elem) {
-    console.debug('Configurator.setTarget(elem) => elem: ', elem);
-    var g_div, o_div;
-    // two target divs for the olmap and googlemap
-    // TODO: use a directive or link function to select the elements
-    // DOM selection or manipulation should not occur in a service
-    g_div = $(elem).find('#gmtest')[0];
-    o_div = $(elem).find('#oltest')[0];
-    // create the maps
-    gmap = new google.maps.Map(g_div, gmap_options);
-    omap = new ol.Map(omap_options);
-
-    view = new ol.View({
-      center: Design.map_details.center,
-      projection: 'EPSG:4326',
-    });
-
-    // set the omap view
-    omap.setView(view);
-    // set the target of the openlayers map
-    omap.setTarget(o_div);
-    // shove OL map into Google's ControlPosition div
-    o_div.parentNode.removeChild(o_div);
-    gmap.controls[google.maps.ControlPosition.TOP_LEFT].push(o_div);
-
-    // initialize the map
-    this.map = {
-      omap: omap,
-      gmap: gmap,
-    }
-    // TODO: be prepared to fix projection of OLmap for zoom < 17 (currently disallowed by map_options)
-    gmap.addListener('projection_changed', function(){
-      // the proj_changed, now fix the projection of the layers
-      gmap_projection = gmap.getProjection();
-      // resize the target element
-      omap.updateSize();
-      google.maps.event.trigger(gmap, 'resize');
-    });
-  }
-*/
