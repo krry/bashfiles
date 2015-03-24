be528ecc436fc4471e3bf8e8f5e1f4d2c547b48f
Jesse LaRue <lazaruslarue@gmail.com>
2015-03-23 23:24:33 -0700

    fixup! setting up for sharelink

--- src/js/stages/share_link.js
+++ src/js/stages/share_link.js
@@ -1,30 +1,14 @@
 angular.module('share_link',[]).config(["$stateProvider", function ($stateProvider) {
-  var stageName,
-      templateUrl,
-      session_ref_key;
 
-  // specifics for for this state
-  stageName = 'design_link';
   // state definition
-  templateUrl = "templates/";
-  // var stageUrl = templateUrl + "/stages/" + stageName + '/';
 
-  $stateProvider.state("design_link", {
-    url: "/oda/:session_ref_key",
+  $stateProvider.state("share_proposal", {
+    url: "/share/:design_key/:bill/:utilityRate/:sctyRate",
     views: {
       'main@': {
-        templateUrl: templateUrl +'design_link.html',
-        controllerAs: "oda",
-        controller: function design_link_ctrl ($stateParams, $state, Clientstream, User) {
-          var vm = this;
-          var user_id,
-              session_key;
-          session_ref_key = $stateParams.session_ref_key;
-          vm.loadUserSession = function(){
-            Clientstream.emit('ODA: Request session',session_ref_key);
-            $state.go('flannel.home');
-          }
-        },
+        templateUrl: 'templates/stages/share/share_proposal.html',
+        controllerAs: "share-proposal",
+        controller: 'ShareCtrl',
       },
     },
   })
