0e1ffdf383cf4102cba882643713ce79415a9574
Jesse LaRue <lazaruslarue@gmail.com>
2015-04-05 03:29:32 -0700

    add ODA string to ODA tools

--- src/js/controllers/DevCtrl.js
+++ src/js/controllers/DevCtrl.js
@@ -3,12 +3,23 @@
   the dev tools controller
 ================================================== */
 
-controllers.controller("DevCtrl", ["$scope", "Clientstream", "Form", DevCtrl_]);
+controllers.controller("DevCtrl", ["$scope", "Clientstream", "Form",  "Session", DevCtrl_]);
 
-function DevCtrl_($scope, Client, Form) {
+function DevCtrl_($scope, Client, Form,  Session) {
   var vm = this;
   vm.prospect = Form.prospect;
+  vm.design_obj = {};
   Client.listen('Form: Loaded', subscribeForm);
+
+  // Client.listen('Session: Loaded', subscribeSession);
+  Client.listen('Design: Loaded', subscribeDesign);
+  function subscribeDesign(data) {
+    vm.design_obj.design_id = data.design_id;
+    vm.design_obj.oda_link = 'http://localhost:8100/#/oda/'+data.design_id;
+    vm.design_obj.zoom_level = data.streams.zoom.subscribe(function(zoom_val) {})
+    vm.design_obj.center = data.streams.center.subscribe(function(center_val) {})
+    vm.design_obj.areas = data.streams.areas.subscribe(function(center_val) {})
+  }
 
   vm.resetForm = function resetForm () {
     Client.emit('Dev: Reset form');
