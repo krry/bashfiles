8182ef47f5778c572ad94d083695d60789e2da6e
lazarus <mr.spothawk@gmail.com>
2015-04-14 18:22:54 -0700

    cleanup

--- src/js/controllers/OverViewCtrl.js
+++ src/js/controllers/OverViewCtrl.js
@@ -10,8 +10,7 @@ controllers.controller('OverViewCtrl', ['Clientstream', 'defaultValues', '$state
 
 function OverViewCtrl_ (Client, defaultValues, $stateParams, Proposal, FIREBASE_URL) {
 
-var vm = this;
-var i = 1;
+  var i = 1;
 
   var map_options = {
     zoom: 20,
@@ -24,24 +23,40 @@ var i = 1;
     draggable: false,
   };
 
-      var ref = new Firebase(FIREBASE_URL).child('designs');
-      var data;
+  var _ref = new Firebase(FIREBASE_URL).child('designs');
 
-       ref.limitToLast(10).on('child_changed', function (ds) {
-        data = ds.exportVal();
-        if(data.areas != null && data.areas[0].ridge != null && data.areas[0].tilt != null)
-        {
-          var pmap = new google.maps.Map(document.getElementById('gmap'+i), map_options);
-          google.maps.event.addDomListener(document.getElementById('gmap'+i), 'click', showPrompt);
-          function showPrompt() {
-            prompt("Copy to clipboard: Ctrl+C, Enter", data.areas[0].wkt + ' lat:' + data.map_details.center.lat + ' lng:' + data.map_details.center.lng + ' tilt:' + data.areas[0].tilt + ' ridge:' + data.areas[0].ridge[0] + ' ' + data.areas[0].ridge[1]);
-          };
-          Proposal.setTargetOverView(data, pmap, i);    
-          i++;
-          if(i == 7)
-          {
-            i = 1;
-          }
-        }
-      });
+  _ref.limitToLast(10).on('child_changed', function (ds) {
+    var data, pmap;
+    data = ds.exportVal();
+
+    if (data.areas !== null && data.areas[0].ridge !== null && data.areas[0].tilt !== null) {
+      pmap = new google.maps.Map(document.getElementById('gmap'+i), map_options);
+      document.getElementById('gmap'+i).removeEventListener('click')
+      google.maps.event.addDomListener(document.getElementById('gmap'+i), 'click', showPrompt);
+
+      function showPrompt() {
+        var prompt_string = [data.areas[0].wkt ,
+                             ' lat:' ,
+                             data.map_details.center.lat ,
+                             ' lng:' ,
+                             data.map_details.center.lng ,
+                             ' tilt:' ,
+                             data.areas[0].tilt ,
+                             ' ridge:' ,
+                             data.areas[0].ridge[0] ,
+                             ' ' ,
+                             data.areas[0].ridge[1]].join(' ');
+
+        prompt("Copy to clipboard: Ctrl+C, Enter", prompt_string );
+      };
+
+      Proposal.setTargetOverView(data, pmap, i);
+
+      // only allow 6 map divs on the page
+      i++;
+      if (i == 7) {
+        i = 1;
+      }
+    }
+  });
 }
