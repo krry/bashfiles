552370a7ec64c90d74e18167b6ba03c3b9eaa6df
mkicinski <mkicinski@gmail.com>
2015-04-15 11:15:54 -0700

    bug fixes for overview

--- src/js/controllers/OverViewCtrl.js
+++ src/js/controllers/OverViewCtrl.js
@@ -12,7 +12,7 @@ function OverViewCtrl_ (Client, defaultValues, $stateParams, Proposal, FIREBASE_
 
 var vm = this;
 var i = 1;
-
+var localdata = [];
   var map_options = {
     zoom: 20,
     mapTypeId: google.maps.MapTypeId.SATELLITE,
@@ -23,24 +23,41 @@ var i = 1;
     keyboardShortcuts: false,
     draggable: false,
   };
-
-      var ref = new Firebase(FIREBASE_URL).child('designs');
+      //var ref = new Firebase(FIREBASE_URL).child('designs');
+      var ref = new Firebase('https://scty-prod.firebaseio.com').child('designs');
       var data;
 
        ref.limitToLast(10).on('child_changed', function (ds) {
         data = ds.exportVal();
-        if(data.areas != null && data.areas[0].ridge != null && data.areas[0].tilt != null)
+        if(data.areas != null)
         {
-          var pmap = new google.maps.Map(document.getElementById('gmap'+i), map_options);
-          google.maps.event.addDomListener(document.getElementById('gmap'+i), 'click', showPrompt);
-          function showPrompt() {
-            prompt("Copy to clipboard: Ctrl+C, Enter", data.areas[0].wkt + ' lat:' + data.map_details.center.lat + ' lng:' + data.map_details.center.lng + ' tilt:' + data.areas[0].tilt + ' ridge:' + data.areas[0].ridge[0] + ' ' + data.areas[0].ridge[1]);
-          };
-          Proposal.setTargetOverView(data, pmap, i);    
-          i++;
-          if(i == 7)
+          if(data.areas[0].ridge != null) 
           {
-            i = 1;
+            if(data.areas[0].tilt != null)
+            {
+
+              localdata[i] = data;
+              var pmap = new google.maps.Map(document.getElementById('gmap'+i), map_options);
+              document.getElementById('gmap'+i).removeEventListener('click')
+              google.maps.event.addDomListener(document.getElementById('gmap'+i), 'click', showPrompt);
+
+
+              var listener = google.maps.event.addListener(pmap, "idle", function() { 
+                pmap.setZoom(20); 
+                google.maps.event.removeListener(listener); 
+              });
+
+              function showPrompt() {
+                mydata = localdata[parseInt(this.id.replace('gmap', ''))]
+                prompt("Copy to clipboard: Ctrl+C, Enter", mydata.areas[0].wkt + ' lat:' + mydata.map_details.center.lat + ' lng:' + mydata.map_details.center.lng + ' tilt:' + mydata.areas[0].tilt + ' ridge:' + mydata.areas[0].ridge[0] + ' ' + mydata.areas[0].ridge[1]);
+              };
+              Proposal.setTargetOverView(data, pmap, i);    
+              i++;
+              if(i == 7)
+              {
+                i = 1;
+              }
+            }
           }
         }
       });
