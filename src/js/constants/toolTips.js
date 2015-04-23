83f2d188bb85978848c2a3d4fa627d2a69a7ca96
ericsond <ericsond@gmail.com>
2015-04-23 06:20:09 -0700

    Configure tooltip language.

--- src/js/constants/toolTips.js
+++ src/js/constants/toolTips.js
@@ -1,6 +1,6 @@
 constants.constant('TOOL_TIP_TEXT', [
-  "Click one corner of your roof to start",
-  "Outline a single field of your roof by clicking the next corner",
-  "Keep going! You're doing great!",
-  "Finish this step by clicking the first point",
+  "Click a corner of your roof to start",
+  "Click to continue outlining an area",
+  "Click to continue outlining an area",
+  "Click the first point to close an area",
 ]);
