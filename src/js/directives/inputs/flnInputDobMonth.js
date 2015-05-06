2da8f2196a2ab886b609a2ad19db12d7d13c57f7
Kerry Snyder <krry@me.com>
2015-05-05 17:20:24 -0700

    invoking dob directives as attributes rather than elements

--- src/js/directives/inputs/flnInputDobMonth.js
+++ src/js/directives/inputs/flnInputDobMonth.js
@@ -2,7 +2,6 @@ directives.directive('flnInputDobMonth', [flnInputDobMonth_]);
 
 function flnInputDobMonth_ () {
   return {
-    restrict: "E",
     templateUrl: "templates/directives/inputs/flnInputDobMonth.html",
     require: '^flnForm',
   };
